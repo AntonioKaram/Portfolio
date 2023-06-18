3baef4dd7f58cb6bf90980fc602a9ca3de900ed2
Antonio Karam <antonio-karam@outlook.com>
2023-06-18 19:06:47 -0400

    spell checks

--- lib/info.tsx
+++ lib/info.tsx
@@ -15,13 +15,13 @@ export const bio = () => {
     <>
       I am passionate about <b>cybersecurity</b> and <b>software engineering</b>.
       My goal is to engineer resource-efficient
-      solutions that withstand the test time. In the tech industry, that means
+      solutions that withstand the test of time. In the tech industry, that means
       building adaptable and scalable solutions. <br />
       <br />
       What drives my passion towards the intersection of those fileds is their
       incorporation of all the components of our tech-driven world: hardware,
       software, and networks. This means that I get to use my
-      experience in different fields, from <b>programing</b>, to
+      experience in different fields, from <b>programming</b>, to
       <b> electronics</b>, <b>computer security</b>, <b>web development</b>, and{" "}
       <b>network protocols</b>, to solve problems in a unique and sustainable
       way.
